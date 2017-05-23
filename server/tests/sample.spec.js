371ee30a2ca1e7cd93eb246e6b7fc8c068db26b3
Delores Diei <delores.diei@andela.com>
2017-05-23 12:47:42 +0100

    complete chotes

---
+++ server/tests/sample.spec.js
@@ -0,0 +1,8 @@
+// import { expect } from 'chai';
+const expect = require("chai").expect;
+
+describe('sample test', () => {
+  it("should return true", () => {
+    expect(true).to.equal(true);
+  })
+})
