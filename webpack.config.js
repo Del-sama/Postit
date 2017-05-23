193cd490b2f7f47f7279c71b4e4d65187cf0c528
Delores Diei <delores.diei@andela.com>
2017-05-23 13:03:15 +0100

    troubleshoot circleci

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,46 @@
-import webpack from 'webpack';
-import path from 'path';
+// import webpack from 'webpack';
+// import path from 'path';
 
-module.exports = {
-  entry: [
-    './client/src/js/index.jsx'
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        loader: 'babel',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.jsx$/,
-        loader: 'babel',
-        exclude: /node_modules/
-      },
-        {
-        test: /\.(png|jpg|jpeg)$/,
-        loaders: ['url']
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  output: {
-    path: path.join(__dirname, 'client/dist/'),
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: './client/dist',
-    hot: true
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    // new webpack.NoErrorsPlugin()
-  ]
-};
+// module.exports = {
+//   entry: [
+//     './client/src/js/index.jsx'
+//   ],
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.js?$/,
+//         loader: 'babel',
+//         exclude: /node_modules/
+//       },
+//       {
+//         test: /\.jsx$/,
+//         loader: 'babel',
+//         exclude: /node_modules/
+//       },
+//         {
+//         test: /\.(png|jpg|jpeg)$/,
+//         loaders: ['url']
+//       },
+//       {
+//         test: /\.scss$/,
+//         loaders: ['style', 'css', 'sass']
+//       }
+//     ]
+//   },
+//   resolve: {
+//     extensions: ['', '.js', '.jsx']
+//   },
+//   output: {
+//     path: path.join(__dirname, 'client/dist/'),
+//     filename: 'bundle.js'
+//   },
+//   devServer: {
+//     contentBase: './client/dist',
+//     hot: true
+//   },
+//   plugins: [
+//     new webpack.optimize.OccurenceOrderPlugin(),
+//     new webpack.HotModuleReplacementPlugin(),
+//     // new webpack.NoErrorsPlugin()
+//   ]
+// };
