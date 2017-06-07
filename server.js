21ab1fa3887d4b5ee17e8926cf0acfc50771cbaa
Delores Diei <delores.diei@andela.com>
2017-06-07 14:25:19 +0100

    feature(user-signup): initialize firebase [145842589]

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ import express from 'express';
 
 const app = express();
 
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+const config = {
+  apiKey: "AIzaSyDXPPQv_oOWkbLwgFUOPmtNYlkt5u6aNy4",
+    authDomain: "postit-733a4.firebaseapp.com",
+    databaseURL: "https://postit-733a4.firebaseio.com",
+    projectId: "postit-733a4",
+    storageBucket: "postit-733a4.appspot.com",
+    messagingSenderId: "1045252909222"
+};
+firebase.initializeApp(config);
+
 app.get('/', (req, res) => {
   res.send("Hello world")
 });
