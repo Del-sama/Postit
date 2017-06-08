8c6d841ca37bc32b51b33712a00fdd089afa659c
Delores Diei <delores.diei@andela.com>
2017-06-08 13:20:16 +0100

    Revert "146280187 Update profile"

--- server.js
+++ server.js
@@ -1,28 +1,11 @@
-require('dotenv').config()
-
 import express from 'express';
-import * as firebase from "firebase";
-import usersRoute from './server/routes/user';
-
-const port = process.env.PORT;
 
 const app = express();
 
-const config = {
-  apiKey: "AIzaSyDXPPQv_oOWkbLwgFUOPmtNYlkt5u6aNy4",
-  authDomain: "postit-733a4.firebaseapp.com",
-  databaseURL: "https://postit-733a4.firebaseio.com",
-  projectId: "postit-733a4",
-  storageBucket: "postit-733a4.appspot.com",
-  messagingSenderId: "1045252909222"
-};
-firebase.initializeApp(config);
+app.get('/', (req, res) => {
+  res.send("Hello world")
+});
 
-app.use('/users', usersRoute);
-
-app.use('/*', (req, res) => {
-  res.send('welcome!')
-})
-const server = app.listen(port, () => {
-  console.log(`Server started. Listening on port ${port}`);
+app.listen(3000, ()=> {
+  console.log("app is listening on port 3000");
 });
