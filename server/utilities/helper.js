94c9593ad0444e3020dd7f5a56f9620a0849b846
Bolaji Olajide <bolaji.olajide@andela.com>
2017-06-13 10:57:53 +0100

    refactor helper function
    refactor the update profile helper function to expect only important arguments such as email, username, profile pictures

--- server/utilities/helper.js
+++ server/utilities/helper.js
@@ -1,12 +1,11 @@
 import * as firebase from "firebase";
 
-const updateUser= (userId, userName, profilePicture) => {
+const updateUser= (userId, userName, profilePicture, email) => {
   const database = firebase.database();
     database.ref(`users/${userId}`).set({
       userName: userName,
       profilePicture: profilePicture,
-      email: email,
-      groups
+      email: email
     })
 }
 
